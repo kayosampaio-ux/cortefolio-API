bd2f6d6a625e654345b99bf43523c1d61e6d5f09
KywzzS <kayo.sampaio@ba.estudante.senai.br>
2026-06-05 20:22:13 -0300

    Add database port configuration

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,7 @@ const connection = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
+  port: process.env.DB_PORT
 });
 
 module.exports = connection;
